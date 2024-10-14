12dc8a7ec59eb3e2b46f6f5fe73dec983b7708ba
Fabiusuario <132870193+Fabiusuario@users.noreply.github.com>
2024-10-13 23:14:36 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 from flask import Flask, request
-
 from view import home, kprototipes_view, errorDatabase
 from controllerK_prototipes import init_kprototipes
+from pyngrok import ngrok
+
 # Crear la instancia de Flask
 app = Flask(__name__)
 
@@ -21,7 +22,11 @@ def kprototipes():
         return kprototipes_view(data)
 
 
+# Configurar ngrok para conectarse al puerto 5000
+public_url = ngrok.connect(5000)
+print("ngrok tunnel URL:", public_url)
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
 
     
